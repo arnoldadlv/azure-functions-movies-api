93143b98a8628ef0651d23fc74d102c2b906d58d
Arnold De La Vega <arnoldd@device-0004.local>
2024-09-07 12:47:36 -0700

    final changes

--- function_app.py
+++ function_app.py
@@ -2,10 +2,12 @@ import azure.functions as func
 import logging
 import json
 from azure.data.tables import TableServiceClient
+from azure.core.credentials import AzureNamedKeyCredential
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 #Get Movies
+@app.function_name(name="GetMovies")
 @app.route(route="GetMovies")
 @app.table_input(arg_name="movies", table_name="Movies", connection="AzureWebJobsStorage")
 def GetMovies(req: func.HttpRequest, movies) -> func.HttpResponse:
@@ -16,10 +18,8 @@ def GetMovies(req: func.HttpRequest, movies) -> func.HttpResponse:
         movies_json 
         
     )
-#Sets up TableServiceClient
-connection_string = "UseDevelopmentStorage=true"
-connection = TableServiceClient.from_connection_string(conn_str=connection_string)
 #Get Movies by Year
+@app.function_name(name="GetMoviesByYear")
 @app.route(route="GetMoviesByYear")
 @app.table_input(arg_name="movies", table_name="Movies",connection="AzureWebJobsStorage")
 def GetMoviesByYear(req: func.HttpRequest, movies) -> func.HttpResponse: 
@@ -32,31 +32,52 @@ def GetMoviesByYear(req: func.HttpRequest, movies) -> func.HttpResponse:
             "Please enter a year", status_code=400
         )
     else:
-        filter_string = f"ReleaseYear eq '{year}'"
-        table_client=connection.get_table_client("Movies")
-        results=table_client.query_entities(query_filter=filter_string)
-        results_list = [dict(result) for result in results]
-        results_json = json.dumps(results_list)
+        filtered_movies = [movie for movie in movies ['ReleaseYear'] == year]
+        filtered_movies_json = json.dumps(filtered_movies)
         return func.HttpResponse(
-            results_json
+            filtered_movies_json
         )
     
     #Get Movies by Genre
+@app.function_name(name="GetMoviesByGenre")
 @app.route(route="GetMoviesByGenre")
-@app.table_input(arg_name="genre", table_name="Movies", connection="AzureWebJobsStorage")
-def GetMoviesByGenre(req: func.HttpRequest, genre) -> func.HttpResponse:
+@app.table_input(arg_name="movies", table_name="Movies", connection="AzureWebJobsStorage")
+def GetMoviesByGenre(req: func.HttpRequest, movies) -> func.HttpResponse:
     logging.info('Running GetMoviesbyGenre')
     genre = req.params.get('genre')
     #Checks if genre parameter exists, if not, returns a status code 400
     if not genre:
         return func.HttpResponse("Please enter a genre", status_code=400)
     else:
-        genre_filter_string = f"Genre eq '{genre}'"
-        table_client=connection.get_table_client("Movies")
-        results=table_client.query_entities(query_filter=genre_filter_string)
-        """for result in results:
-            logging.info(f"Query results: {result}")"""
-        results_list= [dict(result) for result in results]
-        results_json = json.dumps(results_list)
-        return func.HttpResponse(results_json)
-   
+        filtered_movies = [movie for movie in movies if movie['Genre'] == genre]
+        filtered_movies_json = json.dumps(filtered_movies)
+        return func.HttpResponse(filtered_movies_json)
+
+connection_string="AzureWebJobsStorage"
+#Create Movie
+@app.function_name(name="CreateMovie")
+@app.route(route="CreateMovie")
+@app.table_output(arg_name="movie", table_name="Movies", connection="AzureWebJobsStorage")
+def CreateMovie(req: func.HttpRequest, movie) -> func.HttpResponse:
+    logging.info('running func CreateMovie')
+    movie = req.get_json()
+    if not movie:
+        return func.HttpResponse("Please enter a movie", status_code=400)
+    else:
+        account_name = "dlvmoviesfunctionapi"
+        endpoint = "https://dlvmoviesfunctionapi.table.core.windows.net/Movies"
+        account_key ="F2Nblgu9nIVh8kYMJTxcZEmXZG8VIZ7P0O6YDGrTJ/s/ppF2/cM2nnnx+fE3Ug3jT/UVvLdNPo6a+AStEm/2EA=="
+        credential = AzureNamedKeyCredential(name=account_name, key=account_key)
+        service = TableServiceClient(endpoint=endpoint,credential=credential)
+        moviestable = service.get_table_client("Movies")
+        movie_data = {
+            "PartitionKey": movie["ReleaseYear"],
+            "RowKey": movie["Genre"],
+            "Title": movie["Title"],
+            "Genre": movie["Genre"],
+            "coverURL": movie["coverURL"],
+            "ReleaseYear": movie["ReleaseYear"]
+        }
+        moviestable.create_entity(movie_data)
+
+        return func.HttpResponse("Movie added successfully!", status_code=201)
